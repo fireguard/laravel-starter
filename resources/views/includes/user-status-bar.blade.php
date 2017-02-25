c6e6db84f577b35bc706a7a6181fc0a1b1b5ed74
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2017-02-25 16:32:55 -0300

    Change backgroud user image in status bar

--- resources/views/includes/user-status-bar.blade.php
+++ resources/views/includes/user-status-bar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar user panel -->
 <div class="user-panel">
     <div class="pull-left image">
-        <img src="{{ asset('images/user-avatar.png') }}" class="img-circle" alt="User Image">
+        <img src="{{ asset('images/user-avatar.png') }}" class="img-circle" alt="Usuário">
     </div>
     <div class="pull-left info">
         <p>Ronaldo Meneguite</p>
