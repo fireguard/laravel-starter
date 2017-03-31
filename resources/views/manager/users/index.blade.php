6af9aa5d257f45453547d9e43f7402470cd74ac3
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2017-03-31 18:26:10 -0300

    Update Layout for list users

--- resources/views/manager/users/index.blade.php
+++ resources/views/manager/users/index.blade.php
@@ -15,7 +15,7 @@
     <div class="box">
         <div class="box-header with-border">
             <h3 class="box-title"><i class="fa fa-users"></i> Usuários</h3>
-            <div class="box-tools pull-right">
+            <div class="box-tools pull-right hidden-xs">
                 <form>
                     <div class="input-group input-group-sm" style="width: 240px;">
                         <input type="text" value="{{request('search')}}" name="search" id="data-table-search" class="form-control pull-right" placeholder="Pesquisar">
@@ -29,36 +29,38 @@
         </div>
         <div class="box-body">
             <div class="row">
-                <div class="col-xs-12">
-                    <table class="table table-hover table-condensed" >
-                        <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Nome</th>
-                            <th>Email</th>
-                            <th>Função</th>
-                            <th class="text-center">Ações</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($users as $user)
-                            <tr id="row-{{ $user->id }}" class="row-dynamic-table" ondblclick="openRemotePage('{{ route('manager.users.edit', ['id' => $user->id]) }}')">
-                                <td>{{ $user->id }}</td>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->function }}</td>
-                                <td style="width: 140px;">
-                                    <a onclick="openRemotePage('{{ route('manager.users.edit', ['id' => $user->id]) }}')" class="btn btn-warning btn-xs">
-                                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar
-                                    </a>
-                                    <a onclick="removeDataTableItem('{{ route('manager.users.remove', ['id' => $user->id]) }}', {{$user->id}})"
-                                       class="btn btn-danger btn-xs"
-                                    ><i class="fa fa-times-circle" aria-hidden="true"></i> Excluir</a>
-                                </td>
+                <div class="col-xs-12 ">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-condensed" >
+                            <thead>
+                            <tr>
+                                <th style="min-width: 40px;"><span class="legend-icon"><i class="fa fa-search "></i></span> Id</th>
+                                <th><span class="legend-icon"><i class="fa fa-search "></i></span> Nome</th>
+                                <th><span class="legend-icon"><i class="fa fa-search "></i></span> Email</th>
+                                <th><span class="legend-icon"><i class="fa fa-search "></i></span> Função</th>
+                                <th class="text-center">Ações</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr id="row-{{ $user->id }}" class="row-dynamic-table" ondblclick="openRemotePage('{{ route('manager.users.edit', ['id' => $user->id]) }}')">
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->function }}</td>
+                                    <td style="width: 140px;">
+                                        <a onclick="openRemotePage('{{ route('manager.users.edit', ['id' => $user->id]) }}')" class="btn btn-warning btn-xs">
+                                            <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar
+                                        </a>
+                                        <a onclick="removeDataTableItem('{{ route('manager.users.remove', ['id' => $user->id]) }}', {{$user->id}})"
+                                           class="btn btn-danger btn-xs"
+                                        ><i class="fa fa-times-circle" aria-hidden="true"></i> Excluir</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
