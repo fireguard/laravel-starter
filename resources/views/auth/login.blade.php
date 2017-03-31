587caf3b35c64b0b049f7a8cc930d35598abf406
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2017-03-31 18:13:26 -0300

    Refactoring login page layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('manager.layouts.app')
 
 @section('content')
-    <div class="login-box">
+    <div class="login-box" style="max-width: 100%">
         <div class="logo" style="padding: 5px 0; ">
             <div class="logo-lg">
                 <div style="">
@@ -19,37 +19,45 @@
 
             <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                 {{ csrf_field() }}
-                <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input id="email" type="email" placeholder="E-mail" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
-                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input id="password" type="password" class="form-control" placeholder="Senha" name="password" required>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
                 <div class="row">
-                    <div class="col-xs-8">
-                        <div class="checkbox">
+                    <div class="col-xs-12">
+                        <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <input id="email" type="email" placeholder="E-mail" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+
+                        <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <input id="password" type="password" class="form-control" placeholder="Senha" name="password" required>
+                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-8">
+                        <div class="checkbox form-group ">
                             <label>
                                 <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Mantenha-me conectado
                             </label>
+
                         </div>
                     </div>
                     <!-- /.col -->
-                    <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+                    <div class="visible-xs">&nbsp;</div>
+                    <div class="col-xs-12 col-sm-4">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+                        </div>
                     </div>
                     <!-- /.col -->
                 </div>
